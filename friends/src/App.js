32e1763dbd3c697423ea9aa40d466bc639a8e1a4
Lambda_School_Loaner_85 <lambda_school_loaner_85@Lambda-School-Loaner-85s-MacBook-Pro.local>
2019-04-02 15:48:41 -0500

    Setting up data retrieval

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,8 +1,32 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Route } from 'react-router-dom';
+
+import axios from 'axios'
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      friends: [],
+      postSuccessMessage: "",
+      postError: "",
+      deleteSuccessMessage: "",
+      deleteError: "",
+      updateSuccessMessage: "",
+      updateError: "",
+    }
+  }
+  getFriends = () => {
+    axios.get('http://localhost:5000/friends')
+         .then(res => {
+           this.setState({ friends: res.data })
+         })
+         .catch(err => console.log(err));
+  }
+  
   render() {
     return (
       <div className="App">
